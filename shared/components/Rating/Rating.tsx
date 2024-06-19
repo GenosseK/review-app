49908907a99582c30182636fabe7021d683a8bb8
GenosseK <akazhanov72@yandex.ru>
2024-06-19 18:05:36 +0500

    Form error handling, validation and submitting

--- shared/components/Rating/Rating.tsx
+++ shared/components/Rating/Rating.tsx
@@ -5,7 +5,7 @@ import cn from 'classnames';
 import { ForwardedRef, forwardRef, KeyboardEvent, useEffect, useState } from 'react';
 import StarIcon from './star.svg';
 
-export const Rating = forwardRef(({ isEditable = false, rating, setRating, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+export const Rating = forwardRef(({ isEditable = false, error, rating, setRating, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
 	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
 	useEffect(() => {
@@ -58,8 +58,11 @@ export const Rating = forwardRef(({ isEditable = false, rating, setRating, ...pr
 	};
 
 	return (
-		<div {...props} ref={ref}>
+		<div {...props} ref={ref} className={cn(styles.ratingWrapper, {
+			[styles.error]: error
+		})}>
 			{ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+			{error && <span className={styles.errorMessage}>{error.message}</span>}
 		</div>
 	);
 });
